0180440c5b90ddaa9323afb6e9e77860bc4a1b2e
tomWhilbert <thomaswhilbert@gmail.com>
2019-10-22 10:51:16 -0400

    remove duplicate dotfiles subfolder

---
+++ vader/backup-scripts/configBackupss.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+target=/Users/thomaswhilbert/dotFiles/vader
+
+cp /Users/thomaswhilbert/Library/Application\ Support/Code/User/settings.json $target
+cp /Users/thomaswhilbert/.zshrc $target
+cp /Users/thomaswhilbert/.zshrc_aliases $target
+cp /Users/thomaswhilbert/.p10k.zsh $target
+cp /Users/thomaswhilbert/Brewfile $target
