db9b4ab6a3c5958f01d1f0f8a1afa647977acd22
tomWhilbert <uxi9000@gmail.com>
2019-10-26 14:50:40 -0400

    again

--- MBP2019/launchd-config-files/configBackup.sh
+++ MBP2019/launchd-config-files/configBackup.sh
@@ -5,9 +5,6 @@ target=/Users/thomaswhilbert/dotFiles
 cp /Users/thomaswhilbert/Library/Application\ Support/Code/User/settings.json $target
 cp /Users/thomaswhilbert/.zshrc $target
 cp /Users/thomaswhilbert/.zshrc_aliases $target
-cp /Users/thomaswhilbert/.zsh_plugins.sh $target
-cp /Users/thomaswhilbert/.zshrc_aliases $target
-cp /Users/thomaswhilbert/.zsh_plugins.txt $target
 cp /Users/thomaswhilbert/Brewfile $target
-cp /Users/thomaswhilbert/LaunchAgentConfigs/configBackup.sh $target
-cp /Users/thomaswhilbert/LaunchAgentConfigs/configBackup.sh $target
+cp /User/thomaswhilbert/.p10k.zsh $target
+
