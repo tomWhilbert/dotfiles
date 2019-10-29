694e23b7e9cedc43f8c40a95ad183cd0c9d91250
tomWhilbert <uxi9000@gmail.com>
2019-10-29 11:12:19 -0400

    move dotfiles into branch directory, MBP2019

--- MBP2019/launchd-config-files/configBackup.sh
+++ MBP2019/launchd-config-files/configBackup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-target=/Users/thomaswhilbert/dotFiles
+target=/Users/thomaswhilbert/dotFiles/MBP2019
 
 cp /Users/thomaswhilbert/Library/Application\ Support/Code/User/settings.json $target
 cp /Users/thomaswhilbert/.zshrc $target
