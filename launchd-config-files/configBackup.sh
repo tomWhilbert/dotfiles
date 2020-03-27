31d701558cbbbe0015fc1eef2d6a5ce00f874bef
tomWhilbert <thomaswhilbert@gmail.com>
2020-03-27 12:00:22 -0400

    Dotfiles Backup

---
+++ launchd-config-files/configBackup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#* Backup dotfiles to github
+
+target=/Users/thomaswhilbert/.dotFiles
+
+cp /Users/thomaswhilbert/Library/Application\ Support/Code/User/settings.json $target
+cp /Users/thomaswhilbert/.zshrc $target
+cp /Users/thomaswhilbert/.zshrc_aliases $target
+cp /Users/thomaswhilbert/Brewfile $target
+cp /Users/thomaswhilbert/.p10k.zsh $target
+
+sleep 1
+
+cd $target || exit
+git add .
+git commit -m  "Dotfiles Backup"
+git push origin macOS
+exit
+
+
