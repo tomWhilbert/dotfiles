806bf77db21df08dc74e62d2c53776ffe969eb51
tomWhilbert <thomaswhilbert@gmail.com>
2020-06-01 13:19:04 -0400

    Dotfiles Backup

--- launchd-config-files/configBackup.sh
+++ launchd-config-files/configBackup.sh
@@ -6,7 +6,8 @@ target=/Users/thomaswhilbert/.dotFiles
 cp /Users/thomaswhilbert/Library/Application\ Support/Code/User/settings.json $target
 cp /Users/thomaswhilbert/.zshrc $target
 cp /Users/thomaswhilbert/.zshrc_aliases $target
-cp /Users/thomaswhilbert/Brewfile $target
+cp /Users/thomaswhilbert/.zshrc_functions $target
+cp /Users/thomaswhilbert/.dotfiles/Brewfile $target
 cp /Users/thomaswhilbert/.p10k.zsh $target
 
 sleep 1
